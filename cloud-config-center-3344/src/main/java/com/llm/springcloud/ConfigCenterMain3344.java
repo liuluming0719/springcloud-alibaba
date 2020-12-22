4bb9a4154f4cc821cf16d7dd66ff968dfdde307b
liuluming0719 <614562629@qq.com>
2020-12-22 15:39:01 +0800

    springcloud config分布式配置中心

---
+++ cloud-config-center-3344/src/main/java/com/llm/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,18 @@
+package com.llm.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @description: config配置中心主启动类
+ * @author: LiuLuMing
+ * @createtime: 2020-12-21 15:23
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class,args);
+    }
+}
---
+++ cloud-config-client-3355/src/main/java/com/llm/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,18 @@
+package com.llm.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @description: 端口3355主启动类
+ * @author: LiuLuMing
+ * @createtime: 2020-12-21 17:17
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class,args);
+    }
+}
