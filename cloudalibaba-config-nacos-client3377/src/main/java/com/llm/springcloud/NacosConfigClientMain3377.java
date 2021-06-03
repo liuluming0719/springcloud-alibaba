ffc02977aa1ca7317eb1a66b1e50506b94f5891a
liuluming0719 <614562629@qq.com>
2021-06-03 15:24:30 +0800

    Nacos作为服务注册中心

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/llm/springcloud/NacosConfigClientMain3377.java
@@ -0,0 +1,18 @@
+package com.llm.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @description: 3377主启动类
+ * @author: LiuLuMing
+ * @createtime: 2021-06-01 16:43
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosConfigClientMain3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class,args);
+    }
+}
---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/llm/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,24 @@
+package com.llm.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description: Nacos作为配置controller
+ * @author: LiuLuMing
+ * @createtime: 2021-06-01 16:44
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController
+{
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/config/info")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
